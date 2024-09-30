9d26cd55a2c9ad09dca9a3ef6abe0b30b487eb7a
Alessandro Ricci <a.ricci@unibo.it>
2024-09-30 06:17:01 +0200

    Added BBoM MVC refactoring + extensions

---
+++ src/main/java/mvc_01_basic/ModelInterface.java
@@ -0,0 +1,15 @@
+package mvc_01_basic;
+
+/**
+ * 
+ * Interface exposed by the model.
+ * 
+ * 
+ */
+public interface ModelInterface {
+
+	void update();
+
+	int getState();
+
+}
---
+++ src/main/java/mvc_03_dist_mom/MyRemoteViewMain.java
@@ -0,0 +1,32 @@
+package mvc_03_dist_mom;
+
+import com.rabbitmq.client.*;
+
+public class MyRemoteViewMain {
+
+	private final static String EXCHANGE_NAME = "mvc";
+
+	public static void main(String[] argv) throws Exception {
+
+		MyRemoteView view = new MyRemoteView();
+		view.display();
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+
+		Channel channel = connection.createChannel();
+		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+	    String queueName = channel.queueDeclare().getQueue();
+	    channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			view.notifyModelUpdated(Integer.parseInt(message));
+		};
+	    
+		channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });	    
+		System.out.println("Remote Viewer installed. ");
+	}
+}
